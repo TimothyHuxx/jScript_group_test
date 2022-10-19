6969e2b1fc5fb228a375db0da2222ea537ba471b
adiuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2022-10-19 16:15:16 -0400

    Add new test (#8)

--- test/testFunc.spec.js
+++ test/testFunc.spec.js
@@ -29,5 +29,9 @@ describe("Test suit for convert function", () => {
 
     it("convert(-10001) should return (-10001)", () => {
         expect(convert(-10001)).to.be.equal(-10001);
+
     })
+  
+    it("convert(-29374) should return -47392", () => {
+        expect(convert(-29374)).to.be.equal(-47392);
 })
