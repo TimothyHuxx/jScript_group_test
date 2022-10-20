d5920d258db43c9fe2b7913cfdbb534d7e5799ff
LIliia149 <f.lilly149@gmail.com>
2022-10-20 15:38:16 -0400

    Change data for testcase

--- test/testFunc.spec.js
+++ test/testFunc.spec.js
@@ -2,7 +2,7 @@ const convert = require("../src/testFunc");
 const expect = require("chai").expect;
 
 describe("Test suit for convert function", () => {
-    it("convert(345) should return (543)", () => {
-        expect(convert(345)).to.be.equal(543);
+    it("convert(111) should return (111)", () => {
+        expect(convert(111)).to.be.equal(111);
     })
 })
