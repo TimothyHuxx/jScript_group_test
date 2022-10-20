d6bc8d9520c621dfaef799794ad009494750d829
PrimaMicher <87172113+PrimaMicher@users.noreply.github.com>
2022-10-20 23:31:15 +0300

    add test (lines 33-36) (#18)

--- test/testFunc.spec.js
+++ test/testFunc.spec.js
@@ -5,4 +5,9 @@ describe("Test suit for convert function", () => {
     it("convert(111) should return (111)", () => {
         expect(convert(111)).to.be.equal(111);
     })
+
+    it("convert(-2022) should return (-2022)", () => {
+        expect(convert(-2022)).to.be.equal(-2022);
+    })
 })
+
