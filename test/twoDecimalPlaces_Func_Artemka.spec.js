e87b29d6deaa305de86717d039f41bd7c1a7b8b0
Artem Diuzhikov <a.diuzhikov@gmail.com>
2022-10-21 01:30:06 -0400

    New file with a function + test cases

---
+++ test/twoDecimalPlaces_Func_Artemka.spec.js
@@ -0,0 +1,12 @@
+const twoDecimalPlaces = require("../src/twoDecimalPlaces_Func_Artemka");
+const expect = require("chai").expect;
+
+describe("Test suit for twoDecimalPlaces function", () => {
+    it("twoDecimalPlaces(32.8493) should return (32.84)", () => {
+        expect(twoDecimalPlaces(32.8493)).to.be.equal(32.84);
+    })
+
+    it("twoDecimalPlaces(14.3286) should return (14.32)", () => {
+        expect(twoDecimalPlaces(14.3286)).to.be.equal(14.32);
+    })
+})
