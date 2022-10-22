2dc066ba209d7ed28beb51555048e7c2acd684fb
engineeryulia <engineeryulia@gmail.com>
2022-10-21 18:46:52 -0600

    add new test

---
+++ src/calculateInstallments_Yl.js
@@ -0,0 +1,17 @@
+/*  The function shown below that takes an order amount and splits it into
+ four even installments.
+ For example:100.08 = 25.02, 25.02, 25.02, 25.02 or 50.02 = 12.50, 12.50, 12.50, 12.50
+*/
+
+function calculateInstallments(orderAmount) { 
+  let num = orderAmount * 100;
+  
+  while(num % 4 !== 0) {
+    num--;
+  }
+  return Array.from(Array(4).fill(num / 100 / 4))
+}
+console.log(calculateInstallments(100.08));
+console.log(calculateInstallments(50.02));
+
+  module.exports = calculateInstallments;
