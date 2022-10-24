91642ec84360937de6c73852956d8f7b21ee9d25
Anakondos <Anakondos@gmail.com>
2022-10-24 00:08:28 -0400

    add two files func, specs

--- test/charToASSCIICode_Func_Anton.spec.js
+++ test/charToASSCIICode_Func_Anton.spec.js
@@ -3,6 +3,6 @@ const expect = require("chai").expect;
 
 describe("Test suit for charToASSCIICode function", () => {
     it("charToASSCIICode(['a','b','c','d']) should return ([ { a: 97, b: 98, c: 99, d: 100 } ])", () => {
-        expect(charToASSCIICode(['a','b','c','d'])).to.be.equal([ { a: 97, b: 98, c: 99, d: 100 }]);
+        expect(charToASSCIICode(['a','b','c','d'])).to.eql([ { a: 97, b: 98, c: 99, d: 100 }]);
     })
 }) 
