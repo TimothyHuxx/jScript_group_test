1544d2368719b3d896c32758ca71d5a89b97a8e7
Anton Kondakov <Anakondos@gmail.com>
2022-10-24 00:14:13 -0400

    add new function (#38)
    * add new function
    
    * add two files func, specs

---
+++ test/charToASSCIICode_Func_Anton.spec.js
@@ -0,0 +1,8 @@
+const charToASSCIICode = require("../src/getASCIICode_Func_Anton");
+const expect = require("chai").expect;
+
+describe("Test suit for charToASSCIICode function", () => {
+    it("charToASSCIICode(['a','b','c','d']) should return ([ { a: 97, b: 98, c: 99, d: 100 } ])", () => {
+        expect(charToASSCIICode(['a','b','c','d'])).to.eql([ { a: 97, b: 98, c: 99, d: 100 }]);
+    })
+}) 
