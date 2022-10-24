f59b2e3a570d8a5c4a74ac205af0eb7b7a085b94
Vik Kabelkov <vkabelkov@gmail.com>
2022-10-23 18:55:28 -0700

    Edit

--- test/isPoly_Func_Vik.js
+++ test/isPoly_Func_Vik.js
@@ -1,7 +1,6 @@
-//const test_data = require ('../test_data/test_data.json')
+
 const polyndrom = require('../src/Polyndrom_Func_Vik.js')
 const expect = require ("chai").expect;
-//New Comments
 
 describe('Test suit for checking polyndrom', ()=> {
 const test_data = ['abba','madam','kazak','1221','ehe']
