5783aa5a6b5010cc53401344b1a51c75b72b4c10
Vik1975 <98859030+Vik1975@users.noreply.github.com>
2022-10-23 21:37:34 -0700

    Vik oct 24 (#43)
    * add test
    
    * add comment and tests
    
    * Name Changed
    
    * Edit
    
    * edit changes

--- test/isPaly_Func_Vik.js
+++ test/isPaly_Func_Vik.js
@@ -1,9 +1,9 @@
-//const test_data = require ('../test_data/test_data.json')
-const polyndrom = require('../src/polyndrom.js')
+
+const polyndrom = require('../src/Polyndrom_Func_Vik.js')
 const expect = require ("chai").expect;
 
 describe('Test suit for checking polyndrom', ()=> {
-const test_data = ['abba','madam','kazak']
+const test_data = ['abba','madam','kazak','1221','ehe']
    it('checking for polyndrom', ()=> {
     for (let i = 0; i < test_data.length; i++){
     expect(polyndrom(test_data[i])).to.be.equal(test_data[i]);}
