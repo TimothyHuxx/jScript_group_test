ca53af5464239828f83d0f3cdcd111d46fabf822
Vik Kabelkov <vkabelkov@gmail.com>
2022-10-24 13:33:02 -0700

    Edit Palyndrom

--- src/isPalyndrom_Func_Vik.js
+++ src/isPalyndrom_Func_Vik.js
@@ -1,5 +1,5 @@
-function polyndrom(string){
+function palyndrom(string){
 let  new_string = string.split('').reverse().join('');
   return new_string;
 }
-module.exports = polyndrom;
+module.exports = palyndrom;
