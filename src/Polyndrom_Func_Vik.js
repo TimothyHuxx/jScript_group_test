e06561086f89b0512dd7e921c9f27789ff0aaa75
Vik Kabelkov <vkabelkov@gmail.com>
2022-10-23 21:57:07 -0700

    delete duplicates

--- src/Polyndrom_Func_Vik.js
+++
@@ -1,7 +0,0 @@
-function polyndrom(string){
-  console.debug(`testing value ` + string);
-let  new_string = string.split('').reverse().join('');
-  console.debug(' new value : ' + new_string);
-  return new_string;
-}
-module.exports = polyndrom;
