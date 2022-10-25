34e7e8158d845b66fa71cbb0f47b6a9edfc12f24
Sviatlana Bannikova <atevs2789@gmail.com>
2022-10-25 11:36:07 -0500

    add new function

---
+++ test/sumOfPositive_Func_SBannikova.spec.js
@@ -0,0 +1,13 @@
+const positiveSum = require("../src/sumOfPositive_Func_SBannikova");
+const expect = require("chai").expect;
+
+describe("Test suit for positiveSum function", () => {
+    it("positiveSum([1,2,3,4,5]) should return (15)", () => {
+        expect(positiveSum([1,2,3,4,5])).to.be.equal(15);
+    })
+
+    
+     it("positiveSum([1,-2,3,4,5]) should return (13)", () => {
+        expect(positiveSum([1,-2,3,4,5])).to.be.equal(13);
+    })
+})
