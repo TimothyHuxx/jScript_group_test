67ebdd069fee13680ee3c99b2cfe2cb915924c5a
Olga975 <helga7777@gmail.com>
2022-10-25 09:19:52 -0600

    added two tests

---
+++ test/maxNumber_OlgaA.spec.js
@@ -0,0 +1,11 @@
+const maxNumber = require("../src/findMaxNumber_OlgaA");
+const expect = require("chai").expect;
+
+describe("Test suit for maxNumber function", () => {
+    it("maxNumber([5, 10, 15]) should return (15)", () => {
+        expect(maxNumber([5, 10, 15])).to.be.equal(15);
+    })
+    it("maxNumber([25, 12, 1]) should return (25)", () => {
+        expect(maxNumber([25, 12, 1])).to.be.equal(25);
+    })
+})
