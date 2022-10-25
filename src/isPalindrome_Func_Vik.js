b1fa1cfd3e63ba104c09cb7df73b4058cc07dfd4
Vik1975 <98859030+Vik1975@users.noreply.github.com>
2022-10-25 07:55:42 -0700

    Vik oct 24 (#45)
    * edit
    
    * delete duplicates
    
    * Edit Palyndrom
    
    * Edit One More
    
    * Edit final
    
    * Add empty line.Row #3

--- src/isPalindrome_Func_Vik.js
+++ src/isPalindrome_Func_Vik.js
@@ -1,5 +1,5 @@
-function polyndrom(string){
+function palindrome(string){
 let  new_string = string.split('').reverse().join('');
   return new_string;
 }
-module.exports = polyndrom;
+module.exports = palindrome;
---
+++ test/isPalindrome_Func_Vik.spec.js
@@ -0,0 +1,10 @@
+const palindrome = require('../src/isPalindrome_Func_Vik.js')
+const expect = require ("chai").expect;
+
+describe('Test suit for checking palindrome', ()=> {
+const test_data = ['abba','madam','kazak','1221','ehe']
+   it('checking for palindrome', ()=> {
+    for (let i = 0; i < test_data.length; i++){
+    expect(palindrome(test_data[i])).to.be.equal(test_data[i]);}
+  })
+})
