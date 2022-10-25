1c0cb644bc853ed4888374d9ee29b7c6fecabc9b
Mikhail Anikin <zelebmike@gmail.com>
2022-10-25 16:04:01 -0700

    a few more tests

--- test/abbrevName_Func_Mike.spec.js
+++ test/abbrevName_Func_Mike.spec.js
@@ -9,4 +9,10 @@ describe("Test suit for abbrevName function", () => {
     it("abbrevName('Misha Anikin') should return ('M.A.')", () => {
         expect(abbrevName('Misha Anikin')).to.be.equal('M.A');
     })
+    it("abbrevName('Lavrenty Beria') should return ('L.B.')", () => {
+        expect(abbrevName('Lavrenty Beria')).to.be.equal('L.B');
+    })
+    it("abbrevName('Peter Nalich') should return ('P.N.')", () => {
+        expect(abbrevName('Peter Nalich')).to.be.equal('P.N');
+    })
 })
