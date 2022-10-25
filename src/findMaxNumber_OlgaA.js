80d996753bca33e99f2ad9c89be0af29655dc5cd
Olga975 <helga7777@gmail.com>
2022-10-25 09:51:25 -0600

    changed function name

--- src/findMaxNumber_OlgaA.js
+++ src/findMaxNumber_OlgaA.js
@@ -1,5 +1,5 @@
-function maxNumber(array) {
+function maxNumber_func_OlgaAdams(array) {
     return Math.max.apply(Math, array);
 }
   
-  module.exports = maxNumber;
+  module.exports = maxNumber_func_OlgaAdams;
