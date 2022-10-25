abfa2466fae4f9a04e3f6a7f5a04de3474aac63c
Olga975 <helga7777@gmail.com>
2022-10-25 10:07:55 -0600

    test added

--- test/maxNumber_OlgaA.spec.js
+++ test/maxNumber_OlgaA.spec.js
@@ -8,4 +8,7 @@ describe("Test suit for maxNumber function", () => {
     it("maxNumber([25, 12, 1]) should return (25)", () => {
         expect(maxNumber([25, 12, 1])).to.be.equal(25);
     })
+    it("maxNumber([40, 40, 5]) should return (40)", () => {
+        expect(maxNumber([40, 40, 5])).to.be.equal(40);
+    })
 })
