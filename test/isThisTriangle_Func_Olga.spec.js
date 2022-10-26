0ba08ba1d7f7209d45e6cd440298764d246ee36c
OlgaAutomation4 <115113181+OlgaAutomation4@users.noreply.github.com>
2022-10-25 22:01:43 -0700

    Fix my spec (#68)

--- test/isThisTriangle_Func_Olga.spec.js
+++ test/isThisTriangle_Func_Olga.spec.js
@@ -2,11 +2,7 @@ const isTriangle = require("../src/isThisTriangle_Func_Olga");
 const expect = require("chai").expect;
 
 describe("Test suit for isTriangle function", () => {
-    it("isTriangle(-12, 2, -6) should return (true)", () => {
-        expect(isTriangle(-12, 2, -6)).to.be.equal(true);
+    it("isTriangle(12, 2, 6) should return (false)", () => {
+        expect(isTriangle(12, 2, 6)).to.be.equal(false);
     })
 })
-
-function isTriangle(a,b,c) {
-    return a + b > c && a + c > b && c + b > a;
- }
