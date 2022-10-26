5043c8d98176c7aff392ff50a08fdd760074704a
VolhaParmanchuk <chispa85@mail.ru>
2022-10-25 19:33:24 -0700

    added function description in src  file

--- src/generateIntegers_func_Volha.js
+++ src/generateIntegers_func_Volha.js
@@ -1,3 +1,6 @@
+/* function accepts two arguments and generates a sequence 
+containing the integers from the first argument to the second inclusive.*/
+
 function generateIntegers(m, n) {
     let arr = [];
     for (let i = m; i<=n; i++) {
