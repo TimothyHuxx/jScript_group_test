93f51aac21cbcbe7a4615aaa6c0abdf751167272
Venera <vmoldalieva5@gmail.com>
2022-10-27 00:01:39 -0400

    mocha

--- src/ageCalculatorVenera.js
+++
@@ -1,8 +0,0 @@
-let ageCalculator = 1993;
-let currentYear = 2022;
-let possibleAge1 = currentYear - ageCalculator;                                                              
-let possibleAge2 = possibleAge1 - 1;    
-
-
-console.log(`They are either ${possibleAge1} or ${possibleAge2}`);
-// output: They are either 30 or 29
--- test/ageCalculatorVeneraTest.spec.js
+++
@@ -1,8 +0,0 @@
-const ageCalculator = require("../src/ageCalculatorVenera");
-const expect = require("chai").expect;
-
-describe("Test suit for  ageClaculator", () => {
-    it("ageCalculator should output (29 or 30 )", () => {
-        expect (ageCalculator()).to.be.equal(29);
-    })
-})
